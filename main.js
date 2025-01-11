9d13159aa982d5b2cb310e28fd2d73a894f1f6bf
TavSingh <tavparsadsingh2@gmail.com>
2025-01-10 18:55:42 -0800

    Added and tested more packages

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import colors from "colors";
 
 const array = [1, 3, 4, 5, 1, 2, 2, 3, 33, 5, 6]
 const unique = lodash.uniq(array);
-console.log(chalk.bgGray(unique));
+console.log(chalk.gray(unique));
 
 console.log(chalk.blueBright("Hello, with chalk"));
 
